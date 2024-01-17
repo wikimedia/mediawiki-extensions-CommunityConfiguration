8b7af1255662cd75ddda7ff6aeaaa7bb23e6ed01
Cyndy <csimiyu@wikimedia.org>
2024-01-17 18:11:01 +0300

    Add schema resolver logic to make schema definitions relative to extension directory

--- src/Validation/SchemaResolver.php
+++ src/Validation/SchemaResolver.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extension\CommunityConfiguration\Validation;
 
+use http\Exception\RuntimeException;
+
 class SchemaResolver {
 
 	/**
@@ -29,14 +31,28 @@ class SchemaResolver {
 	 * @param string $schema
 	 * @return string|null
 	 */
-	public function resolvePath( string $schema ): ?string {
-		$path = $schema;
-		// If the $schema definition does not start with "/" assume it is
-		// relative to the extensions directory
-		if ( !str_starts_with( $path, '/' ) ) {
-			$path = $GLOBALS[ 'wgExtensionDirectory' ] . '/' . $path;
+	public function resolvePath( string $schema, string $targetDirName ): ?string {
+		$currentDir = __DIR__;
+		// avoid endless traversal if target directory is not found
+		$maxDepth = 10;
+		for ( $depth = 0; $depth < $maxDepth; $depth++ ) {
+			if ( basename( $currentDir ) === $targetDirName ) {
+				break;
+			}
+			$parentDir = dirname( $currentDir );
+			if ( $parentDir === $currentDir ) {
+				// Root directory reached but target directory not found
+				throw new RuntimeException( "Unable to find the `{$targetDirName}` directory." );
+			}
+			$currentDir = $parentDir;
 		}
-		if ( !realpath( $path ) ) {
+
+		if ( basename( $currentDir ) !== $targetDirName ) {
+			throw new RuntimeException(
+				" `{$targetDirName}` directory not found within the maximum search depth." );
+		}
+		$path = realpath( $currentDir . '/' . ltrim( $schema, '/' ) );
+		if ( $path === false || !is_file( $path ) ) {
 			return null;
 		}
 		return $path;
