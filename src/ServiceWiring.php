758beca5bb1bafc32629ec31dd3e781a93b4a8bd
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-02-22 19:31:37 +0100

    Allow registering providers via a config variable
    This makes it possible to register a provider via LocalSettings.php,
    or within PHPUnit tests (which is what this patch is meant
    to support primarily).
    
    Bug: T358263
    Change-Id: I2d9d6dc06101fc7dffe9e845119c575e8aaa478f

--- src/ServiceWiring.php
+++ src/ServiceWiring.php
@@ -11,13 +11,28 @@ return [
 	'CommunityConfiguration.ProviderFactory' => static function ( MediaWikiServices $services ) {
 		$ccServices = CommunityConfigurationServices::wrap( $services );
 		return new ConfigurationProviderFactory(
+			// This is meant to use MainConfig for all CONSTRUCTOR_OPTIONS, with the exemption of
+			// CommunityConfigurationProviders, which is merged from extension.json attributes
+			// section and the MainConfig's version of the variable (in the event of a key
+			// conflict, MainConfig has precedence).
 			new ServiceOptions(
 				ConfigurationProviderFactory::CONSTRUCTOR_OPTIONS,
-				$services->getMainConfig(),
+
+				// NOTE: ServiceOptions merges all passed Config instances together (with the
+				// restriction of CONSTRUCTOR_OPTIONS kept in mind); when a key is present in
+				// multiple Config instances, the _first_ one takes precedence. To ensure the
+				// merged version of CommunityConfigurationProviders gets used, the HashConfig
+				// needs to come first.
 				new HashConfig( [
-					'CommunityConfigurationProviders' => ExtensionRegistry::getInstance()
-						->getAttribute( 'CommunityConfigurationProviders' )
-				] )
+					'CommunityConfigurationProviders' => array_merge(
+						// NOTE: Unlike ServiceOptions, array_merge gives precedence to the
+						// _last_ array. Consequently, the MainConfig version needs to come second.
+						ExtensionRegistry::getInstance()
+							->getAttribute( 'CommunityConfigurationProviders' ),
+						$services->getMainConfig()->get( 'CommunityConfigurationProviders' )
+					)
+				] ),
+				$services->getMainConfig(),
 			),
 			$ccServices->getStoreFactory(),
 			$ccServices->getValidatorFactory(),
