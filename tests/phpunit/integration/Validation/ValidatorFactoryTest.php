7ed46c28360fc86cbb8f22af75c47c14709d3c9d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-02-24 22:58:36 +0100

    Add structure tests
    Ensure all configured stores and validators can be instanciated.
    This serves as an structure test. Verify non-existent
    validators/stores cannot be executed.
    
    Change-Id: I2ca3e73df04ae7636e469381b7988ad55aa7e2b9

---
+++ tests/phpunit/integration/Validation/ValidatorFactoryTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace MediaWiki\Extension\CommunityConfiguration\Tests;
+
+use InvalidArgumentException;
+use MediaWiki\Extension\CommunityConfiguration\CommunityConfigurationServices;
+use MediaWiki\Extension\CommunityConfiguration\Validation\IValidator;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @coversDefaultClass \MediaWiki\Extension\CommunityConfiguration\Validation\ValidatorFactory
+ */
+class ValidatorFactoryTest extends MediaWikiIntegrationTestCase {
+
+	private ?array $validatorSpecs = null;
+
+	private const VALIDATOR_EXTRA_ARGS = [
+		'jsonschema' => [ 'schema.json' ],
+	];
+
+	private function getValidatorSpecs(): array {
+		if ( !$this->validatorSpecs ) {
+			$this->validatorSpecs = $this->getServiceContainer()->getMainConfig()
+				->get( 'CommunityConfigurationValidators' );
+		}
+		return $this->validatorSpecs;
+	}
+
+	/**
+	 * @covers ::newValidator
+	 * @return void
+	 */
+	public function testConstructValidator() {
+		$factory = CommunityConfigurationServices::wrap( $this->getServiceContainer() )
+			->getValidatorFactory();
+
+		foreach ( $this->getValidatorSpecs() as $validatorName => $_ ) {
+			$this->assertInstanceOf(
+				IValidator::class,
+				$factory->newValidator(
+					$validatorName, $validatorName,
+					self::VALIDATOR_EXTRA_ARGS[$validatorName] ?? []
+				)
+			);
+
+			// Ensure multiple validators of the same name can be instanciated
+			$this->assertInstanceOf(
+				IValidator::class,
+				$factory->newValidator(
+					$validatorName . '-second', $validatorName,
+					self::VALIDATOR_EXTRA_ARGS[$validatorName] ?? []
+				)
+			);
+		}
+	}
+
+	/**
+	 * @covers ::newValidator
+	 * @return void
+	 */
+	public function testNonexistentValidator() {
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Validator nonexistent is not supported' );
+
+		CommunityConfigurationServices::wrap( $this->getServiceContainer() )
+			->getValidatorFactory()
+			->newValidator( 'nonexistent', 'nonexistent', [] );
+	}
+
+	/**
+	 * @covers ::getSupportedKeys
+	 */
+	public function testSupportedKeys() {
+		$this->assertSame(
+			array_keys( $this->getValidatorSpecs() ),
+			CommunityConfigurationServices::wrap( $this->getServiceContainer() )
+				->getValidatorFactory()
+				->getSupportedKeys()
+		);
+	}
+}
