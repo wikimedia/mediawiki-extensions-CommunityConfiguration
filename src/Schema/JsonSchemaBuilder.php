68804304940e0d79671f4b99fdcbff987a4e7947
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-04-10 21:19:16 +0200

    [refactor] Introduce JsonSchemaReader
    More places than the builder will need to work with
    schemas, such as the schema migrations.
    
    Code that reads a JsonSchema class should be either
    in JsonSchemaReader (for CC-specific stuff) or
    in Core's ReflectionSchemaSource (for generic
    schema-building stuff).
    
    Bug: T357532
    Change-Id: Ib9f74547106371134fc2f068bde6d2dde9a5a5f6

--- src/Schema/JsonSchemaBuilder.php
+++ src/Schema/JsonSchemaBuilder.php
@@ -2,46 +2,27 @@
 
 namespace MediaWiki\Extension\CommunityConfiguration\Schema;
 
-use InvalidArgumentException;
-use MediaWiki\Settings\Source\ReflectionSchemaSource;
-use ReflectionClass;
-use ReflectionException;
 use stdClass;
 
 class JsonSchemaBuilder implements SchemaBuilder {
 
-	private string $className;
+	private JsonSchemaReader $jsonSchema;
 
-	public function __construct( string $className ) {
-		$this->className = $className;
-	}
-
-	private function getSettingsSource(): ReflectionSchemaSource {
-		return new ReflectionSchemaSource( $this->className );
+	public function __construct( JsonSchemaReader $jsonSchema ) {
+		$this->jsonSchema = $jsonSchema;
 	}
 
 	/**
 	 * @inheritDoc
-	 * @throws ReflectionException
 	 */
 	public function getRootSchema(): array {
-		$reflectionClass = new ReflectionClass( $this->className );
-		if ( !$reflectionClass->isSubclassOf( JsonSchema::class ) ) {
-			throw new InvalidArgumentException(
-				__CLASS__ . ' needs to be used with a class that implements '
-					. JsonSchema::class . '.'
-			);
-		}
-
-		$schemaUriConstant = $reflectionClass->getReflectionConstant( 'SCHEMA_URI' );
-		$schemaVersionConstant = $reflectionClass->getReflectionConstant( 'VERSION' );
+		$this->jsonSchema->assertIsJsonSchema();
 
 		return array_merge( [
-			'$schema' => $schemaUriConstant ? $schemaUriConstant->getValue() : JsonSchema::SCHEMA_URI,
-			'$id' => str_replace( '\\', '/', $this->className ) . '/'
-				. ( $schemaVersionConstant ? $schemaVersionConstant->getValue() : JsonSchema::VERSION ),
+			'$schema' => $this->jsonSchema->getJsonSchemaVersion(),
+			'$id' => $this->jsonSchema->getSchemaId(),
 			JsonSchema::ADDITIONAL_PROPERTIES => false,
-		], $this->getSettingsSource()->loadAsSchema() );
+		], $this->jsonSchema->getReflectionSchemaSource()->loadAsSchema() );
 	}
 
 	/**
