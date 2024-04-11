2fb5317d80002c3661a26ed0502da94b157cdf34
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-04-11 15:42:03 +0200

    Remove unused private method in JsonSchemaValidator
    This should probably be caught by CI...
    
    Change-Id: Iee9621d38a835a748e99bd351243e84fbcb3778d

--- src/Validation/JsonSchemaValidator.php
+++ src/Validation/JsonSchemaValidator.php
@@ -2,13 +2,11 @@
 
 namespace MediaWiki\Extension\CommunityConfiguration\Validation;
 
-use InvalidArgumentException;
 use JsonSchema\Validator;
 use MediaWiki\Extension\CommunityConfiguration\Schema\JsonSchemaBuilder;
 use MediaWiki\Extension\CommunityConfiguration\Schema\SchemaBuilder;
 use Status;
 use StatusValue;
-use stdClass;
 
 /**
  * JSON Schema validator.
@@ -29,47 +27,6 @@ class JsonSchemaValidator implements IValidator {
 	 */
 	public function getSchemaBuilder(): ?SchemaBuilder {
 		return $this->jsonSchemaBuilder;
-	}
-
-	/**
-	 * Is an array associative?
-	 *
-	 * For the purpose of this helper function, any array with non-numeric keys is considered to
-	 * be an associative array.
-	 *
-	 * @param array $array
-	 * @return bool
-	 */
-	private function isArrayAssociative( array $array ): bool {
-		// PHP documentation requires all array keys to be either integers or strings. This means
-		// there can be no key that is not a string and also not a number. Hence, if there is a
-		// non-zero number of string keys, the array must be associative (as defined in the
-		// documenting comment).
-		return count( array_filter( array_keys( $array ), 'is_string' ) ) > 0;
-	}
-
-	/**
-	 * Recursively convert an associative array into a std class
-	 *
-	 * @param array $config
-	 * @return stdClass
-	 * @throws InvalidArgumentException when $config is not an associative array
-	 */
-	private function arrayToStdClass( array $config ): stdClass {
-		if ( !$this->isArrayAssociative( $config ) ) {
-			throw new InvalidArgumentException(
-				__METHOD__ . ' can only process associative arrays.'
-			);
-		}
-
-		$res = new stdClass();
-		foreach ( $config as $key => $value ) {
-			if ( is_array( $value ) && $this->isArrayAssociative( $value ) ) {
-				$value = $this->arrayToStdClass( $value );
-			}
-			$res->$key = $value;
-		}
-		return $res;
 	}
 
 	/**
