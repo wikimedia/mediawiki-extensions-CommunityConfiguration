4ae687969eefa58a58d604e87a3c89ea3c249478
Sergio Gimeno <sgimeno@wikimedia.org>
2024-04-18 14:27:44 +0200

    [refactor] Factor out ui subschema construction
    This will be used for the array control
    to generate the labels for each element
    in an array.
    
    Bug: T360472
    Change-Id: I0c092be979fc2374260797011cc6e95730c34372

--- resources/ext.communityConfiguration.Editor/lib/json-form/core/uischema.js
+++ resources/ext.communityConfiguration.Editor/lib/json-form/core/uischema.js
@@ -2,35 +2,45 @@ const { getControlTextProps } = require( './i18n.js' );
 
 /**
  * Generates a data structure to hold information about the
- * UI the form should display.
+ * UI the form should display. Only type object is supported as
+ * top level type of the schema.
  *
  * @param {Object} schema An object representing a JSON schema
  * @param {Object} config The editor form configuration
  * @param {string} initialScope The schema parent node defined as a "scope" string
+ * @param {Object} data The configuration data for the given schema
  * @return {{ elements: Array<{ name: string, type: string }> }} An array
  * of elements describing the form controls for the properties in the schema.
  */
-function buildUISchema( schema, config, initialScope = '' ) {
+function buildUISchema( schema, config, initialScope = '', data = {} ) {
 	const elements = [];
 	for ( const prop in schema.properties ) {
-		const defaults = {
-			name: prop,
-			type: 'Control',
-			required: schema.required && schema.required.indexOf( prop ) !== -1,
-			scope: initialScope ? `${initialScope}/properties/${prop}` : `#/properties/${prop}`
-		};
-
-		if ( Array.isArray( schema.required ) ) {
-			defaults.required = schema.required.indexOf( prop ) !== -1;
-		}
-
-		const additionalProps = Object.assign( {}, getControlTextProps( prop, config.i18nPrefix ) );
-
-		elements.push( Object.assign( {}, defaults, additionalProps ) );
+		const scope = initialScope ? `${initialScope}/properties/${prop}` : `#/properties/${prop}`;
+		const required = schema.required && schema.required.indexOf( prop ) !== -1;
+		elements.push( buildUISubSchema(
+			schema.properties[ prop ], prop, scope, required, config.i18nPrefix, data[ prop ]
+		) );
 	}
+
 	return { elements };
 }
 
+function buildUISubSchema( subschema, name, scope, required, i18nPrefix, data ) {
+	const defaults = {
+		scope,
+		name,
+		required,
+		type: 'Control'
+	};
+
+	const additionalProps = Object.assign(
+		{}, getControlTextProps( name, i18nPrefix, subschema, data )
+	);
+
+	return Object.assign( {}, defaults, additionalProps );
+}
+
 module.exports = exports = {
-	buildUISchema
+	buildUISchema,
+	buildUISubSchema
 };
