16d18ff03c654afb06b933c94a2de7fc46a62b56
Martin Urbanec <martin.urbanec@wikimedia.cz>
2024-06-06 09:24:00 +0200

    Translate special page names
    Credits:
    * Arabic: Dyolf77_WMF
    * Czech: Martin Urbanec
    * French: Trizek_WMF
    * Spanish: IZapico-WMF
    * Indonesian: BAPerdana-WMF
    
    Bug: T365150
    Change-Id: I93814acf39d7c60867aa19e6748e5900e4e8ec2d

--- CommunityConfiguration.alias.php
+++ CommunityConfiguration.alias.php
@@ -7,6 +7,31 @@ $specialPageAliases['en'] = [
 	'CommunityConfiguration' => [ 'CommunityConfiguration' ],
 ];
 
+/** Arabic */
+$specialPageAliases['ar'] = [
+	'CommunityConfiguration' => [ 'إعدادات_المجتمع' ],
+];
+
+/** čeština (Czech) */
+$specialPageAliases['cs'] = [
+	'CommunityConfiguration' => [ 'Komunitní_konfigurace' ],
+];
+
+/** Spanish (español) */
+$specialPageAliases['es'] = [
+	'CommunityConfiguration' => [ 'Configuración_comunitaria' ],
+];
+
+/** French (français) */
+$specialPageAliases['fr'] = [
+	'CommunityConfiguration' => [ 'Configuration_communautaire' ],
+];
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = [
+	'CommunityConfiguration' => [ 'Pengaturan_komunitas' ],
+];
+
 /** Norwegian Bokmål (norsk bokmål) */
 $specialPageAliases['nb'] = [
 	'CommunityConfiguration' => [ 'Fellesskapskonfigurasjon' ],
