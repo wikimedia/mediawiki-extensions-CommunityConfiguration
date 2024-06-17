e55b8d40956ad2b7b664aa17c2ada679494d0604
Michael Große <mgrosse@wikimedia.org>
2024-06-17 13:05:14 +0200

    fix: correctly treat `0` as a minimum value, not as falsy
    Bug: T367721
    Change-Id: I109243bc50504c57798cb484c7b96fc82ca51b83

---
+++ resources/ext.communityConfiguration.Editor/lib/json-form/controls-codex/src/controls/NumberControl.test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { mount } = require( '@vue/test-utils' );
+const { ref } = require( 'vue' );
+const NumberControl = require( './NumberControl.vue' );
+
+describe( 'NumberControl', () => {
+	it( 'renders a number field without min/max if none are given', () => {
+		const uischema = {
+			name: 'NumberFieldName',
+			scope: '#/properties/NumberFieldName',
+			type: 'Control'
+		};
+		const schema = {
+			type: 'number',
+			default: null
+		};
+		const jsonform = {
+			uischema,
+			renderers: null,
+			schema,
+			data: {},
+			errors: ref( [] )
+		};
+
+		const wrapper = mount( NumberControl, {
+			props: {
+				renderers: null,
+				uischema,
+				schema
+			},
+			global: {
+				provide: {
+					jsonform
+				}
+			}
+		} );
+
+		expect( wrapper.get( 'input[type="number"]' ).attributes( 'min' ) ).toBe( undefined );
+		expect( wrapper.get( 'input[type="number"]' ).attributes( 'max' ) ).toBe( undefined );
+	} );
+
+	it( 'renders a number field with min/max if they are given in the schema', () => {
+		const uischema = {
+			name: 'NumberFieldName',
+			scope: '#/properties/NumberFieldName',
+			type: 'Control'
+		};
+		const schema = {
+			type: 'number',
+			minimum: 0,
+			maximum: 10,
+			default: null
+		};
+		const jsonform = {
+			uischema,
+			renderers: null,
+			schema,
+			data: {},
+			errors: ref( [] )
+		};
+
+		const wrapper = mount( NumberControl, {
+			props: {
+				renderers: null,
+				uischema,
+				schema
+			},
+			global: {
+				provide: {
+					jsonform
+				}
+			}
+		} );
+
+		expect( wrapper.get( 'input[type="number"]' ).attributes( 'min' ) ).toBe( '0' );
+		expect( wrapper.get( 'input[type="number"]' ).attributes( 'max' ) ).toBe( '10' );
+	} );
+} );
