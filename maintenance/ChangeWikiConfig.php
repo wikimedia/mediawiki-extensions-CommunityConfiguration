39a59b4b1d970a3afd15772912425a1a45f3a767
Michael Große <mgrosse@wikimedia.org>
2024-08-21 13:31:15 +0200

    Add maint script to change existing configuration
    Bug: T357535
    Depends-On: I6ce345331291d6867aa1441fef85e063cd9664db
    Change-Id: Icc2e3748294c29ad9364e7e2e162d03ba2024b87

---
+++ maintenance/ChangeWikiConfig.php
@@ -0,0 +1,207 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CommunityConfiguration\Maintenance;
+
+use Maintenance;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\CommunityConfiguration\CommunityConfigurationServices;
+use MediaWiki\Extension\CommunityConfiguration\Provider\ConfigurationProviderFactory;
+use MediaWiki\Json\FormatJson;
+use MediaWiki\Language\FormatterFactory;
+use MediaWiki\Maintenance\MaintenanceFatalError;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\User;
+use MessageLocalizer;
+use stdClass;
+
+class ChangeWikiConfig extends Maintenance {
+
+	private ConfigurationProviderFactory $providerFactory;
+	private FormatterFactory $formatterFactory;
+	private MessageLocalizer $messageLocalizer;
+
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'CommunityConfiguration' );
+		$this->addDescription( 'Update a config key in on-wiki config' );
+
+		$this->addOption(
+			'summary',
+			'Edit summary to use',
+			false,
+			true
+		);
+		$this->addOption(
+			'delete',
+			'delete the key if it exists',
+		);
+
+		$this->addOption(
+			'dry-run',
+			'print the config that would be saved and exit',
+		);
+
+		$this->addArg(
+			'provider',
+			'Provider whose data that will be changed',
+			true
+		);
+		$this->addArg(
+			'key',
+			'Config key that is updated (use . to separate keys in an object)',
+			true
+		);
+		$this->addArg(
+			'value',
+			'New value of the config key, must be valid json',
+			false
+		);
+	}
+
+	private function initServices(): void {
+		$services = MediaWikiServices::getInstance();
+		$ccServices = CommunityConfigurationServices::wrap( $services );
+		$this->providerFactory = $ccServices->getConfigurationProviderFactory();
+		$this->formatterFactory = $services->getFormatterFactory();
+		$this->messageLocalizer = RequestContext::getMain();
+	}
+
+	/**
+	 * @inheritDoc
+	 * @throws MaintenanceFatalError
+	 */
+	public function execute(): bool {
+		$this->initServices();
+
+		$provider = $this->providerFactory->newProvider( $this->getArg( 'provider' ) );
+
+		$configStatus = $provider->getStore()->loadConfigurationUncached();
+		if ( !$configStatus->isGood() ) {
+			$this->output( "Failed to load config:\n" );
+			$this->fatalError( $configStatus );
+		}
+		$config = $configStatus->value;
+
+		$key = $this->getArg( 'key' );
+		if ( $this->hasOption( 'delete' ) ) {
+			if ( $this->getArg( 'value' ) !== null ) {
+				$this->fatalError( '"value" argument must not be set when deleting a key!' );
+			}
+			$this->deleteConfigKey( $config, $key );
+		} else {
+			if ( $this->getArg( 'value' ) === null ) {
+				$this->fatalError( '"value" argument must be set when adding a value!' );
+			}
+			$value = $this->getValueFromArg( $this->getArg( 'value' ) );
+
+			$this->setConfigKeyToValue( $config, $key, $value );
+		}
+
+		$user = User::newSystemUser( User::MAINTENANCE_SCRIPT_USER, [ 'steal' => true ] );
+		if ( !$user ) {
+			$this->fatalError( 'Failed to create user' );
+		}
+
+		if ( $this->hasOption( 'dry-run' ) ) {
+			$this->output( "Would save:\n" );
+			$this->output( FormatJson::encode( $config, true ) . "\n" );
+			$validationStatus = $provider->getValidator()->validateStrictly( $config );
+			$this->output( "Validation status:\n" );
+			$this->output( $validationStatus );
+			$this->output( "\n" );
+			return true;
+		}
+
+		$summaryAsWikitext = $this->messageLocalizer->msg(
+			'communityconfiguration-maintenance-config-change-summary'
+		)->params( $this->getOption( 'summary', '' ) )->inContentLanguage()->text();
+		$saveStatus = $provider->getStore()->alwaysStoreConfiguration(
+			$config,
+			null,
+			$user,
+			$summaryAsWikitext
+		);
+		if ( !$saveStatus->isOK() ) {
+			$this->error( $saveStatus );
+			return false;
+		}
+
+		$this->output( "Saved!\n" );
+		return true;
+	}
+
+	/**
+	 * @throws MaintenanceFatalError
+	 * @return mixed The decoded json from the argument
+	 */
+	private function getValueFromArg( string $arg ) {
+		$valueStatus = FormatJson::parse( $arg );
+
+		if ( !$valueStatus->isGood() ) {
+			$parsingError = $this->formatterFactory->getStatusFormatter(
+				$this->messageLocalizer
+			)->getWikiText(
+				$valueStatus
+			);
+			$this->fatalError( '`' . $arg . '` is not valid JSON: ' . $parsingError );
+		}
+
+		return $valueStatus->getValue();
+	}
+
+	/**
+	 * @param stdClass $config
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	private function setConfigKeyToValue( stdClass $config, string $key, $value ): void {
+		if ( !str_contains( $key, '.' ) ) {
+			$config->$key = $value;
+		} else {
+			$this->setObjectSubKey( $config, $key, $value );
+		}
+	}
+
+	/**
+	 * @param stdClass $config
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	private function setObjectSubKey( stdclass $config, string $key, $value ): void {
+		$subKeys = explode( '.', $key );
+		$valueKey = array_pop( $subKeys );
+		$target = &$config;
+		foreach ( $subKeys as $subKey ) {
+			if ( !isset( $target->$subKey ) ) {
+				$target->$subKey = new stdClass();
+			}
+			$target = &$target->$subKey;
+		}
+		$target->$valueKey = $value;
+	}
+
+	private function deleteConfigKey( stdClass $config, string $key ): void {
+		if ( !str_contains( $key, '.' ) ) {
+			unset( $config->$key );
+			return;
+		}
+		[ $propertyKey, $subKeys ] = explode( '.', $key, 2 );
+		if ( is_array( $config->$propertyKey ) ) {
+			$this->deleteArraySubKey( $config->$propertyKey, $subKeys );
+		} else {
+			$this->deleteConfigKey( $config->$propertyKey, $subKeys );
+		}
+	}
+
+	private function deleteArraySubKey( array $config, string $key ): void {
+		foreach ( $config as $arrayElement ) {
+			$this->deleteConfigKey( $arrayElement, $key );
+		}
+	}
+
+}
+
+$maintClass = ChangeWikiConfig::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ tests/phpunit/integration/maintenance/ChangeWikiConfigTest.php
@@ -0,0 +1,244 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\CommunityConfiguration\Tests\Integration;
+
+use IDBAccessObject;
+use MediaWiki\Extension\CommunityConfiguration\CommunityConfigurationServices;
+use MediaWiki\Extension\CommunityConfiguration\Maintenance\ChangeWikiConfig;
+use MediaWiki\Maintenance\MaintenanceFatalError;
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use MediaWiki\Title\Title;
+use stdClass;
+
+/**
+ * @covers \MediaWiki\Extension\CommunityConfiguration\Maintenance\ChangeWikiConfig
+ * @group Database
+ */
+class ChangeWikiConfigTest extends MaintenanceBaseTestCase {
+
+	protected function getMaintenanceClass(): string {
+		return ChangeWikiConfig::class;
+	}
+
+	public static function setValueSuccessCases(): array {
+		return [
+			'string' => [ 'CCExample_String', '"expectedValue"' ],
+			'set full array' => [ 'CCExample_FavoriteColors', '["crimson","firebrick","gold"]' ],
+			'full object' => [ 'CCExample_Numbers', '{"IntegerNumber":7,"DecimalNumber":0.9}' ],
+			'sub-field of object' => [
+				'CCExample_Numbers.IntegerNumber',
+				42,
+				'{}',
+				'{"CCExample_Numbers":{"IntegerNumber":42}}',
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider setValueSuccessCases
+	 */
+	public function testSucceedsSettingValue(
+		string $fieldName,
+		$newValue,
+		string $initialConfigValue = '{}',
+		?string $expectedConfigValue = null
+	): void {
+		if ( $expectedConfigValue === null ) {
+			$expectedConfigValue = "{\"$fieldName\":$newValue}";
+		}
+		$initialEditStatus = $this->editPage( 'MediaWiki:CommunityConfigurationExample.json', $initialConfigValue );
+		$this->assertStatusGood( $initialEditStatus );
+		$this->maintenance->loadParamsAndArgs(
+			null,
+			[ 'summary' => '<custom summary here>' ],
+			[ 'CommunityConfigurationExample', $fieldName, $newValue ]
+		);
+
+		$result = $this->maintenance->execute();
+
+		$this->assertTrue( $result );
+		$actualConfig = $this->getValidConfig();
+		$this->assertEquals( $expectedConfigValue, json_encode( $actualConfig ) );
+		$latestRevision = $this->getServiceContainer()->getRevisionLookup()
+			->getRevisionById(
+				Title::newFromText( 'MediaWiki:CommunityConfigurationExample.json' )
+					->getLatestRevID( IDBAccessObject::READ_LATEST )
+			);
+		$actualSummary = $latestRevision->getComment()->text;
+		$this->assertSame( 'Config changed by maintenance script: <custom summary here>', $actualSummary );
+	}
+
+	public static function deletingValueCases(): iterable {
+		yield 'delete top-level property' => [
+			'{ "CCExample_String": "value" }',
+			"CCExample_String",
+			'{}',
+		];
+
+		yield 'delete sub-field of object' => [
+			'{ "CCExample_Numbers": { "IntegerNumber": 42 } }',
+			"CCExample_Numbers.IntegerNumber",
+			'{"CCExample_Numbers":{}}',
+		];
+
+		yield 'delete sub-field of every object in array' => [
+			'{ "CCExample_RelevantPages": [
+			{ "text": "foo", "title": "Page_1", "id": "Q123" },
+			{ "text": "bar", "title": "Page_2" },
+			{ "text": "baz", "title": "Page_3", "id": "Q456" }
+			] }',
+			"CCExample_RelevantPages.id",
+			'{ "CCExample_RelevantPages": [
+			{ "text": "foo", "title": "Page_1" },
+			{ "text": "bar", "title": "Page_2" },
+			{ "text": "baz", "title": "Page_3" }
+			] }',
+		];
+	}
+
+	/**
+	 * @dataProvider deletingValueCases
+	 */
+	public function testSucceedsDeletingValue(
+		string $initialConfigValue,
+		string $fieldName,
+		string $expectedConfigValue
+	): void {
+		$initialEditStatus = $this->editPage( 'MediaWiki:CommunityConfigurationExample.json', $initialConfigValue );
+		$this->assertStatusGood( $initialEditStatus );
+		$this->maintenance->loadParamsAndArgs(
+			null,
+			[
+				'summary' => '(custom summary here)',
+				'delete' => '',
+			],
+			[ 'CommunityConfigurationExample', $fieldName ]
+		);
+
+		$result = $this->maintenance->execute();
+
+		$this->assertTrue( $result );
+		$actualConfig = $this->getValidConfig();
+		$expectedConfigWithoutWhitespace = preg_replace( '/\s+/', '', $expectedConfigValue );
+		$this->assertEquals( $expectedConfigWithoutWhitespace, json_encode( $actualConfig ) );
+		$latestRevision = $this->getServiceContainer()->getRevisionLookup()
+			->getRevisionById(
+				Title::newFromText( 'MediaWiki:CommunityConfigurationExample.json' )
+					->getLatestRevID( IDBAccessObject::READ_LATEST )
+			);
+		$actualSummary = $latestRevision->getComment()->text;
+		$this->assertSame( 'Config changed by maintenance script: (custom summary here)', $actualSummary );
+	}
+
+	public static function noopCases() {
+		yield 'setting value that exists' => [
+			'{ "CCExample_String": "value" }',
+			"CCExample_String",
+			'"value"',
+		];
+
+		yield 'delete property that does not exist' => [
+			'{ "CCExample_String": "value" }',
+			"NonExistingProperty",
+		];
+	}
+
+	/**
+	 * @dataProvider noopCases
+	 */
+	public function testNoops(
+		string $initialConfigValue,
+		string $fieldName,
+		?string $newValueAsJson = null
+	): void {
+		$isDelete = $newValueAsJson === null;
+
+		$initialEditStatus = $this->editPage(
+			'MediaWiki:CommunityConfigurationExample.json',
+			$initialConfigValue,
+			'summary of initial edit'
+		);
+		$this->assertStatusGood( $initialEditStatus );
+
+		$arguments = [ 'CommunityConfigurationExample', $fieldName ];
+		$options = [ 'summary' => '<custom summary here>' ];
+		if ( $isDelete ) {
+			$options['delete'] = '';
+		} else {
+			$arguments[] = $newValueAsJson;
+		}
+		$this->maintenance->loadParamsAndArgs(
+			null,
+			$options,
+			$arguments
+		);
+
+		$result = $this->maintenance->execute();
+
+		$this->assertTrue( $result );
+		$actualConfig = $this->getValidConfig();
+		$initialConfigValueWithoutWhitespace = preg_replace( '/\s+/', '', $initialConfigValue );
+		$this->assertEquals( $initialConfigValueWithoutWhitespace, json_encode( $actualConfig ) );
+		$latestRevision = $this->getServiceContainer()->getRevisionLookup()
+			->getRevisionById(
+				Title::newFromText( 'MediaWiki:CommunityConfigurationExample.json' )
+					->getLatestRevID( IDBAccessObject::READ_LATEST )
+			);
+		$actualSummary = $latestRevision->getComment()->text;
+		$this->assertSame( 'summary of initial edit', $actualSummary );
+	}
+
+	public function testFailsOnInvalidConfig(): void {
+		$initialEditStatus = $this->editPage( 'MediaWiki:CommunityConfigurationExample.json', '{}' );
+		$this->assertStatusGood( $initialEditStatus );
+		$this->maintenance->loadParamsAndArgs(
+			null,
+			[ 'summary' => '(not actually saved)' ],
+			[ 'CommunityConfigurationExample', 'NonExistingProperty', '"bar"' ]
+		);
+
+		$result = $this->maintenance->execute();
+
+		$this->assertFalse( $result );
+		$actualConfig = $this->getValidConfig();
+		$this->assertEquals( '{}', json_encode( $actualConfig ) );
+		$expectedOutputLines = [
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+			'Error: DRAFT: The property NonExistingProperty is not defined and the definition does not allow additional properties. Key: ',
+		];
+		$this->assertOutputPrePostShutdown( implode( "\n", $expectedOutputLines ) . "\n", false );
+	}
+
+	private function getValidConfig(): stdClass {
+		$provider = CommunityConfigurationServices::wrap( $this->getServiceContainer() )
+			->getConfigurationProviderFactory()
+			->newProvider( 'CommunityConfigurationExample' );
+		$actualConfigStatus = $provider->getStore()->loadConfigurationUncached();
+		$this->assertStatusGood( $actualConfigStatus );
+		$actualConfig = $actualConfigStatus->value;
+		$validationStatus = $provider->getValidator()->validateStrictly( $actualConfig );
+		$this->assertStatusGood( $validationStatus );
+		return $actualConfig;
+	}
+
+	public function testFailsOnInvalidJson(): void {
+		$this->maintenance->loadParamsAndArgs(
+			null,
+			[ 'summary' => '(not actually saved)' ],
+			[ 'CommunityConfigurationExample', 'NotValidJson', '{"bar"' ]
+		);
+
+		try {
+			$this->maintenance->execute();
+		} catch ( MaintenanceFatalError $e ) {
+			$this->assertSame( 1, $e->getCode() );
+		}
+
+		$this->assertOutputPrePostShutdown(
+			"`{\"bar\"` is not valid JSON: Syntax error\n",
+			false
+		);
+	}
+}
