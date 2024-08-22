fde01e234ffd0a113c11f005aae8fc324ddf8e80
Michael Große <mgrosse@wikimedia.org>
2024-08-22 20:17:16 +0200

    docs(example): Add two number fields as part of an object
    This object can subsequently be used in tests for the change-config
    script.
    
    Bug: T372053
    Change-Id: I7e5ce731c235f7efd4e91601806657718d3bf8c6

--- example/src/Specials/SpecialCommunityConfigurationExample.php
+++ example/src/Specials/SpecialCommunityConfigurationExample.php
@@ -26,6 +26,7 @@ class SpecialCommunityConfigurationExample extends SpecialPage {
 		foreach ( $stringConfigNames as $configName ) {
 			$this->showStringConfig( $configName );
 		}
+		$this->showNumbersFromObject( 'CCExample_Numbers' );
 		$this->showMultiselectEnumConfig();
 	}
 
@@ -41,6 +42,20 @@ class SpecialCommunityConfigurationExample extends SpecialPage {
 		foreach ( $colorConfigValues as $color ) {
 			$this->getOutput()->addHTML( '<div style="background-color: ' . $color . ';">&nbsp;</div>' );
 			$this->getOutput()->addWikiTextAsInterface( 'Color: ' . $color );
+		}
+	}
+
+	private function showNumbersFromObject( string $configName ): void {
+		$this->getOutput()->addWikiTextAsInterface( '==== Numbers: ====' );
+		$this->getOutput()->addWikiTextAsInterface(
+			'Note that each values has a default, so there will always be a number here.'
+		);
+		$exampleNumberObject = $this->wikiConfig->get( $configName );
+		$fieldNames = [ 'IntegerNumber', 'DecimalNumber' ];
+		foreach ( $fieldNames as $fieldName ) {
+			$this->getOutput()->addWikiTextAsInterface(
+				$configName . ' ' . $fieldName . ': ' . $exampleNumberObject->{$fieldName}
+			);
 		}
 	}
 
