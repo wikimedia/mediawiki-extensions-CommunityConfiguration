e40a7a0514ab27a9a812d25bb55b851aac5b7435
Sergio Gimeno <sgimeno@wikimedia.org>
2024-09-04 13:18:31 +0200

    CommonsFileControl: use config variable for the commons api url
    For consistency and to facilitate QA, this change configures the
    api url CommonsFileControl uses to request files when searching
    from it. The variable is passed to the client through the
    form config "commonsApiUrl".
    
    Bug: T369069
    Depends-On: I691d742af404c7484e6dfc4c97f184e4e6e79ae9
    Change-Id: Ie9a399e22480f14157275b5a046cc04e52cc3c3b

--- resources/ext.communityConfiguration.Editor/lib/json-form/controls-codex/src/mediawiki/api.test.js
+++ resources/ext.communityConfiguration.Editor/lib/json-form/controls-codex/src/mediawiki/api.test.js
@@ -1,17 +1,30 @@
 'use strict';
-const api = require( './api.js' );
+const { MwForeignApi } = require( './api.js' );
 
-describe( 'API searchCommons', () => {
+describe( 'MwForeignApi searchCommons', () => {
+	it( 'handle missing API URL error', async () => {
+		class FakeApi {
+			get() {
+				return Promise.resolve( {} );
+			}
+		}
+		mw.ForeignApi = FakeApi;
+		mw.errorLogger = { logError: jest.fn() };
+
+		expect( MwForeignApi ).toThrow( '[MwForeignApi] Invalid param baseUrl' );
+		expect( mw.errorLogger.logError ).toHaveBeenCalledTimes( 1 );
+	} );
 	it( 'handle API error', async () => {
-		class fakeApi {
+		class FakeApi {
 			get() {
 				return Promise.reject( 'Some error' );
 			}
 		}
-		mw.ForeignApi = fakeApi;
+		mw.config.get = jest.fn().mockReturnValueOnce( 'some.url' );
+		mw.ForeignApi = FakeApi;
 		mw.errorLogger = { logError: jest.fn() };
 
-		await expect( api.searchCommonsFiles( 'input', 0 ) ).rejects.toThrow(
+		await expect( new MwForeignApi( 'http://some.url' ).searchCommonsFiles( 'input', 0 ) ).rejects.toThrow(
 			'Some error',
 		);
 		expect( mw.errorLogger.logError ).toHaveBeenCalledTimes( 1 );
