a7f5e15f05aa2b75d1b18f3d8934675f614e8edc
Michael Große <mgrosse@wikimedia.org>
2024-10-10 11:52:58 +0200

    tests: abstract test-case for asserting emergency defaults
    Note that it is important that getProvider() is called in the tests and
    not in `setUp()`, because that way extensions have a chance to set any
    configuration options needed.
    
    Bug: T374428
    Follow-up: Ic42f625023f581f086f2e125425f17b33fc4f427
    Follow-up: I3e41795a43243fee5967f242778d677de9425178
    Change-Id: I0615769b19a3d78bd6ba8fc38c69c879fd9f9f7c

---
+++ tests/phpunit/integration/SchemaProviderTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\CommunityConfiguration\Tests;
+
+use MediaWiki\Extension\CommunityConfiguration\CommunityConfigurationServices;
+use MediaWiki\Extension\CommunityConfiguration\Provider\IConfigurationProvider;
+use MediaWikiIntegrationTestCase;
+
+abstract class SchemaProviderTestCase extends MediaWikiIntegrationTestCase {
+
+	/**
+	 * If the provider can be toggled conditionally,
+	 * then use the setUp method to explicitly enable it for this test class.
+	 */
+	abstract protected function getProviderId(): string;
+
+	abstract protected function getExtensionName(): string;
+
+	final public function testSchemaDefaultValues(): void {
+		$provider = $this->getProvider();
+		$actualDefaults = $provider->getValidator()->getSchemaBuilder()->getDefaultsMap();
+
+		$updater = CommunityConfigurationServices::wrap( $this->getServiceContainer() )->getEmergencyDefaultsUpdater();
+		$path = $updater->getDefaultsFileForProvider( $provider, $this->getExtensionName() );
+
+		// phpcs:ignore Generic.Files.LineLength
+		$this->assertFileExists( $path, "Please generate the emergency defaults file $path with the CommunityConfiguration UpdateEmergencyDefaults maintenance script, and check it into your repository." );
+
+		$storedEmergencyDefaults = require $path;
+		$this->assertEquals( $storedEmergencyDefaults, $actualDefaults );
+	}
+
+	final protected function getProvider(): IConfigurationProvider {
+		return CommunityConfigurationServices::wrap( $this->getServiceContainer() )
+			->getConfigurationProviderFactory()
+			->newProvider( $this->getProviderId() );
+	}
+}
