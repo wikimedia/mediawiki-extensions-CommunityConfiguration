1c4acc41577126cb8fc4446e870ca49846d57d2d
Michael Große <mgrosse@wikimedia.org>
2024-10-17 00:38:49 +0200

    tests: ensure maintenance base class has always been requierd
    This fixes the following error in CI when testing in parallel:
    
    00:06:58.122 There was 1 error:
    00:06:58.122
    00:06:58.123 1) MediaWiki\Extension\CommunityConfiguration\Tests\Integration\ChangeWikiConfigTest::testSucceedsSettingValue with data set "string" ('CCExample_String', '"expectedValue"')
    00:06:58.123 Use of undefined constant RUN_MAINTENANCE_IF_MAIN - assumed 'RUN_MAINTENANCE_IF_MAIN' (this will throw an Error in a future version of PHP)
    00:06:58.123
    00:06:58.123 /workspace/src/extensions/CommunityConfiguration/maintenance/ChangeWikiConfig.php:207
    00:06:58.123 /workspace/src/includes/AutoLoader.php:226
    00:06:58.123 /workspace/src/tests/phpunit/maintenance/MaintenanceBaseTestCase.php:65
    00:06:58.123 /workspace/src/tests/phpunit/maintenance/MaintenanceBaseTestCase.php:22
    00:06:58.123 phpvfscomposer:///workspace/src/vendor/phpunit/phpunit/phpunit:106
    
    Probably, requiring maintenance should happen somewhere in the
    maintenance test-class, given that it is no longer needed for execution
    through run.php. But currently this is breaking CI, the proper solution
    can be done later.
    
    Bug: T377391
    Bug: T357535
    Change-Id: Iec06a3d8653085500f2079ed142574b339871a9b

--- maintenance/ChangeWikiConfig.php
+++ maintenance/ChangeWikiConfig.php
@@ -16,6 +16,12 @@ use MediaWiki\User\User;
 use MessageLocalizer;
 use stdClass;
 
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
 class ChangeWikiConfig extends Maintenance {
 
 	private ConfigurationProviderFactory $providerFactory;
