13ad4219b5995b01df8e09e3b4cc5f0931ef9268
Martin Urbanec <martin.urbanec@wikimedia.cz>
2025-05-04 10:25:40 +0300

    [tests] Run ExtensionJsonTest
    Change-Id: I20ab27efa931f01228be1a1b66beb670c773ebdf

---
+++ tests/phpunit/integration/CommunityConfigurationExtensionJsonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MediaWiki\Extension\CommunityConfiguration\Tests;
+
+use MediaWiki\Tests\ExtensionJsonTestBase;
+
+/**
+ * @coversNothing
+ */
+class CommunityConfigurationExtensionJsonTest extends ExtensionJsonTestBase {
+	/** @inheritDoc */
+	protected string $extensionJsonPath = __DIR__ . '/../../../extension.json';
+}
